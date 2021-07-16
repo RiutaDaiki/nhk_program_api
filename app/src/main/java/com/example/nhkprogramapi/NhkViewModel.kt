9fdd36030b7735f9bd33bd7ca951838bbc091b24
大喜理風 <daikiriuta0926@gmail.com>
2021-07-16 11:57:16 +0900

    居住地mapを定義

--- app/src/main/java/com/example/nhkprogramapi/NhkViewModel.kt
+++ app/src/main/java/com/example/nhkprogramapi/NhkViewModel.kt
@@ -21,6 +21,63 @@ class NhkViewModel : ViewModel() {
         2131230809 to "s3"
     )
 
+    val residenceMap = mapOf<String, String>(
+        "札幌" to "010",
+        "函館" to "011",
+        "旭川" to "012",
+        "帯広" to "013",
+        "釧路" to "014",
+        "北見" to "015",
+        "室蘭" to "016",
+        "青森" to "020",
+        "盛岡" to "030",
+        "仙台" to "040",
+        "秋田" to "050",
+        "山形" to "060",
+        "福島" to "070",
+        "水戸" to "080",
+        "宇都宮" to "090",
+        "前橋" to "100",
+        "さいたま" to "110",
+        "千葉" to "120",
+        "東京" to "130",
+        "横浜" to "140",
+        "新潟" to "150",
+        "富山" to "160",
+        "金沢" to "170",
+        "福井" to "180",
+        "甲府" to "190",
+        "長野" to "200",
+        "岐阜" to "210",
+        "静岡" to "220",
+        "名古屋" to "230",
+        "津" to "240",
+        "大津" to "250",
+        "京都" to "260",
+        "大阪" to "270",
+        "神戸" to "280",
+        "奈良" to "290",
+        "和歌山" to "300",
+        "鳥取" to "310",
+        "松江" to "320",
+        "岡山" to "330",
+        "広島" to "340",
+        "山口" to "350",
+        "福島" to "360",
+        "高松" to "370",
+        "松山" to "380",
+        "高知" to "390",
+        "福岡" to "400",
+        "北九州" to "401",
+        "佐賀" to "410",
+        "長崎" to "420",
+        "熊本" to "430",
+        "大分" to "440",
+        "宮崎" to "450",
+        "鹿児島" to "460",
+        "沖縄" to "470"
+    )
+
     val programInfoList = MutableLiveData<List<ProgramInfo>>()
 
     fun getProgramTitle(date: String, service: Int) {
@@ -58,4 +115,6 @@ class NhkViewModel : ViewModel() {
     }
 
     val serviceId = MutableLiveData<Int>()
+
+    val selectedResidence = MutableLiveData<String>()
 }
--- app/src/main/java/com/example/nhkprogramapi/ui/LiveInDialog.kt
+++ app/src/main/java/com/example/nhkprogramapi/ui/LiveInDialog.kt
@@ -3,15 +3,21 @@ package com.example.nhkprogramapi.ui
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.example.nhkprogramapi.NhkViewModel
 import com.example.nhkprogramapi.R
 import com.example.nhkprogramapi.databinding.DialogLiveInBinding
 
-class LiveInDialog: DialogFragment() {
+class LiveInDialog : DialogFragment() {
+    private val viewModel: NhkViewModel by activityViewModels()
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val binding = DialogLiveInBinding.inflate(layoutInflater)
-//        binding.lifecycleOwner = viewLifecycleOwner
 
         val adapter = ArrayAdapter<String>(
             this.requireContext(),
@@ -21,13 +27,31 @@ class LiveInDialog: DialogFragment() {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.liveInSpinner.adapter = adapter
+        binding.liveInSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val spinnerParent = parent as Spinner
+                val item = spinnerParent.selectedItem as String
+                println(item)
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
         val builder = AlertDialog.Builder(context)
             .setView(binding.root)
             .setMessage("居住地を選択")
-            .setPositiveButton("OK"){dialog, which ->
+            .setPositiveButton("OK") { dialog, which ->
 
             }
-            .setNegativeButton("キャンセル"){dialog, which ->
+            .setNegativeButton("キャンセル") { dialog, which ->
                 dialog.dismiss()
             }
 
